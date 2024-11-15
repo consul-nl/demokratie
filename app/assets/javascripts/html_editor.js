82337d341bcb14ed419fd3676a1024cf188cd823
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2024-11-15 11:16:19 +0200

    Fix ckeditor5

--- app/assets/javascripts/html_editor.js
+++ app/assets/javascripts/html_editor.js
@@ -1,10 +1,26 @@
 (function() {
   "use strict";
+
+  function removeWrappingParagraphs(html) {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = html;
+
+    // Remove <p> tags wrapping only <a> elements
+    tempDiv.querySelectorAll('p').forEach(p => {
+      if (p.childNodes.length === 1 && p.firstChild.tagName === 'A') {
+        p.replaceWith(p.firstChild); // Replace <p> with the <a> itself
+      }
+    });
+
+    return tempDiv.innerHTML;
+  }
+
   App.HTMLEditor = {
     initialize: function() {
       var {
         ClassicEditor
       } = CKEDITOR;
+
 
       document.querySelectorAll( 'textarea.html-area' ).forEach( textarea => {
         ClassicEditor.create(textarea, {
@@ -12,6 +28,10 @@
           toolbar: {
             items: this.toolbarFor(textarea).toolbarControls,
             shouldNotGroupWhenFull: true
+          },
+
+          schema: {
+            allow: '$text a', // Allows `<a>` as an inline element
           },
 
           image: {
@@ -30,6 +50,62 @@
             }
           },
 
+          heading: {
+            options: [
+              {
+                model: 'paragraph',
+                title: 'Paragraph',
+                class: 'ck-heading_paragraph'
+              },
+              {
+                model: 'heading1',
+                view: 'h1',
+                title: 'Heading 1',
+                class: 'ck-heading_heading1'
+              },
+              {
+                model: 'heading2',
+                view: 'h2',
+                title: 'Heading 2',
+                class: 'ck-heading_heading2'
+              },
+              {
+                model: 'heading3',
+                view: 'h3',
+                title: 'Heading 3',
+                class: 'ck-heading_heading3'
+              },
+              {
+                model: 'heading4',
+                view: 'h4',
+                title: 'Heading 4',
+                class: 'ck-heading_heading4'
+              },
+              {
+                model: 'heading5',
+                view: 'h5',
+                title: 'Heading 5',
+                class: 'ck-heading_heading5'
+              },
+              {
+                model: 'heading6',
+                view: 'h6',
+                title: 'Heading 6',
+                class: 'ck-heading_heading6'
+              }
+            ]
+          },
+
+          ckfinder: {
+            // Upload the images to the server using the CKFinder QuickUpload command.
+            uploadUrl: 'https://example.com/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images&responseType=json',
+
+            // Define the CKFinder configuration (if necessary).
+            options: {
+              resourceType: 'Images'
+            }
+          },
+
           simpleUpload: {
             uploadUrl: '/ckeditor/pictures',
             withCredentials: true,
@@ -38,7 +114,47 @@
             }
           },
 
+          htmlSupport: {
+            allow: [
+              {
+                name: /.*/,
+                attributes: true,
+                classes: true,
+                styles: true
+              }
+            ]
+          },
+
           extraPlugins: [
+            function(editor) {
+              editor.sourceElement.form.addEventListener("submit", function(e) {
+                e.preventDefault();
+
+                var textarea = editor.sourceElement
+                var editorData = editor.getData();
+                var processedData = removeWrappingParagraphs(editorData);
+
+                textarea.value = processedData
+                textarea.form.submit();
+              })
+
+              editor.data.processor.originalToView =  editor.data.processor.toView;
+              editor.data.processor.toView = (data) => {
+                const viewFragment = editor.data.processor.originalToView(data);
+
+                var domFragment = editor.data.processor.domConverter.viewToDom( viewFragment );
+                var viewFragmentData =  editor.data.processor.htmlWriter.getHtml( domFragment );
+                console.log({viewFragmentData})
+
+                return viewFragment;
+              };
+              editor.data.processor.originalToData =  editor.data.processor.toData;
+              editor.data.processor.toData = (viewFragment) => {
+                var data = editor.data.processor.originalToData(viewFragment)
+                data = removeWrappingParagraphs(data);
+                return data
+              };
+            },
             function ( editor ) {
               // Allow <iframe> elements in the model.
               editor.model.schema.register( 'iframe', {
@@ -85,11 +201,32 @@
               });
             },
           ],
+        }).then(function(editor) {
+              // editor.conversion.for('upcast').elementToElement({
+              //   model: 'a',
+              //   view: 'a',
+              //   converterPriority: 'high'
+              // });
+              //
+              // editor.conversion.for('downcast').elementToElement({
+              //   model: 'a',
+              //   view: 'a',
+              //   converterPriority: 'high'
+              // });
+            console.log(editor.model.schema.getDefinitions())
         })
       });
+
+      CKFinder.basePath = "http://localhost:3000/ckfinder/";
+
+      // CKFinder.widget( 'ckfinder-widget', {
+      //     configPath: '',
+      //     language: 'de'
+      // } );
     },
 
     toolbarFor: function(element) {
+
       var toolbarControls, plugins;
       var {
         ImageBlock,
@@ -104,9 +241,12 @@
         ImageUpload,
         SimpleUploadAdapter,
         ClassicEditor,
+        CKFinder,
+        CKFinderUploadAdapter,
         Essentials,
         Font,
         Paragraph,
+        Heading,
         List,
         Indent,
         IndentBlock,
@@ -128,13 +268,13 @@
         SpecialCharactersEssentials,
         MediaEmbed,
         SourceEditing,
-        HtmlEmbed
+        HtmlEmbed,
+        GeneralHtmlSupport,
       } = CKEDITOR;
 
       if ( $(element).hasClass("extended-u") ) {
-
         plugins = [
-          Essentials, Font, Paragraph,
+          Essentials, Font, Paragraph, Heading,
           List, Indent, IndentBlock, BlockQuote, Alignment,
           Link, Bold, Italic, Underline, Strikethrough, Subscript, Superscript, RemoveFormat,
           Table, HorizontalLine, SpecialCharacters, SpecialCharactersEssentials
@@ -142,7 +282,7 @@
 
         toolbarControls = [
           "bulletedList", "numberedList", "|", "indent", "outdent", "|", "blockQuote", "|", "alignment:left", "alignment:center", "alignment:right", "alignment:justify", "|",
-          "link", "|", "bold", "italic", "underline", "strikethrough", "subscript", "superscript", "|", "removeFormat", "|",
+          "heading", "|", "link", "|", "bold", "italic", "underline", "strikethrough", "subscript", "superscript", "|", "removeFormat", "|",
           "fontColor", "fontBackgroundColor", "|",
           "insertTable", "horizontalLine", "specialCharacters"
         ]
@@ -150,19 +290,21 @@
       } else if ( $(element).hasClass("extended-a") ) {
 
         plugins = [
-          Essentials, Font, Paragraph,
+          Essentials, Font, Paragraph, Heading,
+          CKFinder, CKFinderUploadAdapter,
           List, Indent, IndentBlock, BlockQuote, Alignment,
           ImageBlock, ImageCaption, ImageInline, ImageInsert, ImageResize, ImageStyle, ImageTextAlternative, ImageToolbar, ImageUpload, SimpleUploadAdapter,
           Link, Bold, Italic, Underline, Strikethrough, Subscript, Superscript, RemoveFormat,
           Table, HorizontalLine, SpecialCharacters, SpecialCharactersEssentials,
           MediaEmbed, SourceEditing,
-          HtmlEmbed
+          HtmlEmbed, GeneralHtmlSupport
         ]
 
         toolbarControls = [
           "bulletedList", "numberedList", "|", "indent", "outdent", "|", "blockQuote", "|", "alignment:left", "alignment:center", "alignment:right", "alignment:justify", "|",
           "insertImage", "|",
-          "link", "|", "bold", "italic", "underline", "strikethrough", "subscript", "superscript", "|", "removeFormat", "|",
+          "ckfinder", "|",
+          "heading", "|", "link", "|", "bold", "italic", "underline", "strikethrough", "subscript", "superscript", "|", "removeFormat", "|",
           "fontColor", "fontBackgroundColor", "|",
           "insertTable", "horizontalLine", "specialCharacters", "|",
           "mediaEmbed", "sourceEditing", "|",
