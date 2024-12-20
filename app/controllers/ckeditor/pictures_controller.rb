e0518c9bb18d958d5eee8e2d247f3336bf135a42
Michael Svystun <michael.svystun@gmail.com>
2024-12-20 16:54:45 +0000

    con-2323

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -7,21 +7,39 @@ class Ckeditor::PicturesController < ApplicationController
     picture.attach_uploaded_file(params[:upload])
 
     if picture.save
-      render json: { url: picture.url_content }
+      render json: picture.attributes.symbolize_keys.slice(*allowed_attributes).merge(
+        url: picture.url_content(editor_id: params[:editor_id]),
+        thumb_url: picture.url_thumb
+      )
     else
       render json: { error: { message: picture.errors.messages.values.flatten.join(", ") }}
     end
   end
 
+  def update
+    picture = Ckeditor::Picture.find(params[:id])
+    authorize! :update, picture
+    picture.update!(picture_params)
+    render json: picture.attributes.symbolize_keys.slice(*allowed_attributes).merge(
+      url: picture.url_content(editor_id: params[:editor_id]),
+      thumb_url: picture.url_thumb
+    )
+  end
+
+  def destroy
+    picture = Ckeditor::Picture.find(params[:id])
+    authorize! :destroy, picture
+    picture.destroy!
+    render json: { status: :no_content }
+  end
+
   private
 
     def picture_params
-      {
-        data_file_name: params[:upload].original_filename,
-        data_content_type: params[:upload].content_type,
-        data_file_size: params[:upload].size,
-        type: "Ckeditor::Picture",
-        storage_data: params[:upload].tempfile
-      }
+      params.require(:picture).permit(:title, :description, :alt_text)
+    end
+
+    def allowed_attributes
+      %i[id data_file_name data_content_type data_file_size width height created_at title description alt_text url thumb_url]
     end
 end
